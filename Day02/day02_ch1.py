010ee8b43d6e87616cd18680a073043560c69385
gromuxall <smadon3@uic.edu>
2020-12-04 21:39:04 -0600

    Day 2 Challenge 1/2 Complete

---
+++ Day02/day02_ch1.py
@@ -0,0 +1,34 @@
+import argparse
+
+
+def main():
+    '''
+    Main execution
+    '''
+
+    with open(ARGS.input, 'r') as fp:
+        lines = fp.readlines()
+    valid = 0
+
+    for line in lines:
+        splits = line.split()
+        _min = int(splits[0].split('-')[0])
+        _max = int(splits[0].split('-')[1])
+
+        _char = splits[1].split(':')[0]
+
+        password = splits[2]
+
+        if password.count(_char) >= _min and password.count(_char) <= _max:
+            valid += 1
+    return valid
+
+
+
+# --------------------------------------------------------------------------- //
+if __name__ == "__main__":
+    PARSER = argparse.ArgumentParser()
+    PARSER.add_argument('-i', '--input', required=True, help='input file')
+    ARGS = PARSER.parse_args()
+
+    print(main())
